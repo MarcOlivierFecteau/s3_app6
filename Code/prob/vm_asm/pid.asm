fb4667b7b815a728ff75a827ec9af58d3d4d00c2
Marc-Olivier Fecteau <marc-olivier.fecteau@usherbrooke.ca>
2023-07-19 09:18:50 -0400

    adds constants initialization

--- Code/prob/vm_asm/pid.asm
+++ Code/prob/vm_asm/pid.asm
@@ -1,6 +1,12 @@
 # PID avec K_P = 0.001, K_I = 0.0001, K_D = 0.0001
 # Auteurs: Marc-Olivier Fecteau (FECM0701), Gabriel Aubut (AUBG3402)
 
+LDC R1, 0.001		# R1 = 0.001 (K_P)
+STO $K_P, R1        # mem($K_P) = R1 = K_P
+LDC R2, 0.0001		# R2 = 0.0001 (K_I)
+STO $K_I, R2        # mem($K_I) = R2 = K_I
+LDC R3, 0.0001		# R3 = 0.0001 (K_D)
+STO $K_D, R3        # mem($K_D) = R3 = K_D
 LDA R1,	$V_MOTEUR	# R1 = mem($V_MOTEUR)
 LDA R2, $V_CIBLE	# R2 = mem($V_CIBLE)
 LDA R3, $I_t		# R3 = mem($I_t)
