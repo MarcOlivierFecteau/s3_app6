deb5c5672d97e3dccdef25effc773927fc7623db
Marc-Olivier Fecteau <marc-olivier.fecteau@usherbrooke.ca>
2023-07-19 09:15:33 -0400

    changes characters to MAJ

--- Code/prob/vm_asm/pid.asm
+++ Code/prob/vm_asm/pid.asm
@@ -1,23 +1,23 @@
 # PID avec P = -0.5, I = 0, D = 0, donc sans I ou D
 # Auteurs: Marc-Olivier Fecteau (FECM0701), Gabriel Aubut (AUBG3402)
 
-LDA R1,	$v_moteur	# R1 = mem($v_moteur)
-LDA R2, $v_cible	# R2 = mem($v_cible)
+LDA R1,	$V_MOTEUR	# R1 = mem($V_MOTEUR)
+LDA R2, $V_CIBLE	# R2 = mem($V_CIBLE)
 LDA R3, $I_t		# R3 = mem($I_t)
 LDC R4, 0			# R4 = 0
-SUB R2, R1			# R2 = v_cible(t) - v_mot(t) = e(t)
-LDA R1,	$e_prev		# R1 = mem($e_prev)
+SUB R2, R1			# R2 = V_CIBLE(t) - v_mot(t) = e(t)
+LDA R1,	$E_PREV		# R1 = mem($E_PREV)
 ADD R4, R2			# R4 = R4 + e(t) = e(t)
 ADD R3, R2			# R3 = I(t-1) + e(t) = I(t)
 SUB R4, R1			# R4 = e(t) - e(t-1) = e_d(t)
-STO $e_prev, R2		# mem($e_prev) = R2 = e(t)
-LDA R1, $kp			# R1 = mem($kp)
+STO $E_PREV, R2		# mem($E_PREV) = R2 = e(t)
+LDA R1, $K_P		# R1 = mem($K_P)
 STO $I_t, R3		# mem($I_t) = R3 = I(t)
-MUL R2, R1			# R2 = e(t) * kp
-LDA R1, $ki			# R1 = mem($ki)
-MUL R3, R1			# R3 = I(t) * ki
-LDA R1, $kd			# R1 = mem($kd)
-ADD R2, R3			# R2 = kp * e(t) + ki * I(t)
-MUL R4, R1			# R4 = e_d(t) * kd
-ADD R2, R4			# R2 = kp * e(t) + ki * I(t) + kd * e_d(t) = cmd
-STO $cmd, R2		# mem($cmd) = R2 = cmd
+MUL R2, R1			# R2 = e(t) * K_P
+LDA R1, $K_I		# R1 = mem($K_I)
+MUL R3, R1			# R3 = I(t) * K_I
+LDA R1, $K_D		# R1 = mem($K_D)
+ADD R2, R3			# R2 = K_P * e(t) + K_I * I(t)
+MUL R4, R1			# R4 = e_d(t) * K_D
+ADD R2, R4			# R2 = K_P * e(t) + K_I * I(t) + K_D * e_d(t) = CMD
+STO $CMD, R2		# mem($CMD) = R2 = CMD
